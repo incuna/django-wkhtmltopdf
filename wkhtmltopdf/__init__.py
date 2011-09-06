0fa698a30787eeb5843d3f63620005fcc97ad909
Max Peterson <max@incuna.com>
2011-09-06 12:09:12 +0100

    Fixed init for mkrelease

--- wkhtmltopdf/__init__.py
+++ wkhtmltopdf/__init__.py
@@ -1,4 +1,7 @@
-from .utils import *
-__version__ = (0, 1, 1)
+import os
+if 'DJANGO_SETTINGS_MODULE' in os.environ:
+    from .utils import *
+
+__version__ = (0, 1)
 def get_version():
     return '.'.join(map(str, __version__))
