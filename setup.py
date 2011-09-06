bc15826cec83f85a49c3c9c312184960b8a19a95
Max Peterson <max@incuna.com>
2011-09-06 12:14:15 +0100

    Fixed setup.py to use get_version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages = find_packages(),
     include_package_data=True,
     #install_requires=[],
-    version = "0.1",
+    version = get_version(),
     description = "Converts html to PDF using http://code.google.com/p/wkhtmltopdf/.",
     author = "Incuna Ltd",
     author_email = "admin@incuna.com",
