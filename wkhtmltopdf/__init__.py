c71fc06f47c16cbee56167418eeb31b7150079fc
James Turnbull <james@incuna.com>
2012-03-09 09:34:43 +0000

    Bumped version to 0.2.1

--- wkhtmltopdf/__init__.py
+++ wkhtmltopdf/__init__.py
@@ -2,6 +2,6 @@ import os
 if 'DJANGO_SETTINGS_MODULE' in os.environ:
     from .utils import *
 
-__version__ = (0, 2)
+__version__ = (0, 2, 1)
 def get_version():
     return '.'.join(map(str, __version__))
