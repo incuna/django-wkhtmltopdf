b7290b3df5d4946d2e4444f4c07d9f5ef808213d
Carl Robben <carl.robben@yellow.co.nz>
2012-11-02 11:35:17 +1300

    Catch AttributeError when running under mod_wsgi #13

--- wkhtmltopdf/utils.py
+++ wkhtmltopdf/utils.py
@@ -83,7 +83,11 @@ def wkhtmltopdf(pages, output=None, **kwargs):
                       _options_to_args(**options),
                       list(pages),
                       [output]))
-    return check_output(args, stderr=sys.stderr, env=env)
+    try:
+        return check_output(args, stderr=sys.stderr, env=env)
+    except AttributeError:
+        return check_output(args, env=env)
+
 
 
 def content_disposition_filename(filename):
