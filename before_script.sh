da815660c9a13db44c26c3db6cf88bcd9d66ea47
mattack108 <matt.lenc@gmail.com>
2013-09-20 12:30:20 +0100

    Move it move

--- before_script.sh
+++ before_script.sh
@@ -4,6 +4,6 @@ sudo apt-get install -y openssl build-essential xorg libssl-dev
 wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2
 tar xvjf wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2
 sudo chown root:root wkhtmltopdf-amd64
-sudo ln -s wkhtmltopdf-amd64 /usr/bin/wkhtmltopdf
+sudo mv wkhtmltopdf-amd64 /usr/bin/wkhtmltopdf
 export WKHTMLTOPDF_CMD=/usr/bin/wkhtmltopdf
 $WKHTMLTOPDF_CMD --version
