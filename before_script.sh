1ee1b632cbfc08b5142e4a636c22d1787cdb0ce1
mattack108 <matt.lenc@gmail.com>
2013-09-20 11:45:25 +0100

    Fix command for installing wkhtmltopdf because Travis upgraded to latest OS where aptitude is not available

--- before_script.sh
+++ before_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo aptitude install -y openssl build-essential xorg libssl-dev
+sudo apt-get install -y openssl build-essential xorg libssl-dev
 wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
 tar xvjf wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
 sudo chown root:root wkhtmltopdf-i386
