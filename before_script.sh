169420ceeab025691f67458bc58c7e24d0476756
mattack108 <matt.lenc@gmail.com>
2013-09-20 11:52:13 +0100

    Debug

--- before_script.sh
+++ before_script.sh
@@ -6,3 +6,4 @@ tar xvjf wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
 sudo chown root:root wkhtmltopdf-i386
 sudo mv wkhtmltopdf-i386 /usr/bin/wkhtmltopdf
 export WKHTMLTOPDF_CMD=/usr/bin/wkhtmltopdf
+$WKHTMLTOPDF_CMD --version
