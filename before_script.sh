9bb65daec0a23932c2ab096dddc9ea7c0c5b30dd
mattack108 <matt.lenc@gmail.com>
2013-09-20 12:20:25 +0100

    Are you 64bit now, Travis?

--- before_script.sh
+++ before_script.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y openssl build-essential xorg libssl-dev
-wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
-echo 'untar'
-tar xvjf wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
-echo 'chown'
-sudo chown root:root wkhtmltopdf-i386
-echo 'export'
-export WKHTMLTOPDF_CMD=`pwd`/wkhtmltopdf-i386
+wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2
+tar xvjf wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2
+sudo chown root:root wkhtmltopdf-amd64
+export WKHTMLTOPDF_CMD=`pwd`/wkhtmltopdf-amd64
 $WKHTMLTOPDF_CMD --version
