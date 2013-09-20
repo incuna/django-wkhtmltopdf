45eab831e2ad1f3f73560527ed5f618bfb62177f
mattack108 <matt.lenc@gmail.com>
2013-09-20 11:39:46 +0100

    Fix tests for Travis by obtaining the WKHTMLTOPDF_CMD from env vars.

--- before_script.sh
+++ before_script.sh
@@ -5,4 +5,4 @@ wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-i386.
 tar xvjf wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
 sudo chown root:root wkhtmltopdf-i386
 sudo mv wkhtmltopdf-i386 /usr/bin/wkhtmltopdf
-export WKHTMLTOPDF=/usr/bin/wkhtmltopdf
+export WKHTMLTOPDF_CMD=/usr/bin/wkhtmltopdf
