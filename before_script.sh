6734ba6659bce8282ab2aefa90f910dff071d618
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-03-02 15:26:23 -0300

    other change before_script

--- before_script.sh
+++ before_script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo 'installing dependencies'
+echo '## Installing dependencies'
 sudo apt-get install -y openssl build-essential xorg libssl-dev
-echo 'downloading wkhtmltox'
+echo '## Downloading wkhtmltox'
 wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.0/wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz
-echo 'extracting wkhtmltox'
+echo '## Extracting wkhtmltox'
 tar xvJf wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz
 #sudo chown root:root wkhtmltox
 current_folder=`pwd`
 #sudo mv wkhtmltox /usr/bin/wkhtmltopdf
-export WKHTMLTOPDF_CMD=$current_folder/wkhtmltox
+export WKHTMLTOPDF_CMD=$current_folder/wkhtmltox/bin
