5c7bd1a1e020fd69a4b38c1f78dac6ab8cf8189d
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-03-02 22:20:24 -0300

    changes on Makefile and before_script

--- before_script.sh
+++ before_script.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
+WKHTMLTOX_FOLDER="`pwd`/wkhtmltox_folder"
 
 echo '## Installing dependencies'
-sudo apt-get install -y openssl build-essential xorg libssl-dev
+#sudo apt-get install -y openssl build-essential xorg libssl-dev
 echo '## Downloading wkhtmltox'
 wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.0/wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz
-echo '## Extracting wkhtmltox'
-tar xvJf wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz
+mkdir -p $WKHTMLTOX_FOLDER
+echo '## Extracting wkhtmltox into $WKHTMLTOX_FOLDER'
+tar xvJf wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz -C $WKHTMLTOX_FOLDER
 #sudo chown root:root wkhtmltox
-current_folder=`pwd`
-wkhtmltopdf_binary=$current_folder/wkhtmltox/bin/wkhtmltopdf
-echo $wkhtmltopdf_binary
-#sudo mv wkhtmltox /usr/bin/wkhtmltopdf
-echo "## Exporting WKHTMLTOPDF_CMD system enviroment variable"
-echo "WKHTMLTOPDF_CMD=$wkhtmltopdf_binary"
-export WKHTMLTOPDF_CMD=$wkhtmltopdf_binary
+#sudo mv $WKHTMLTOX_FOLDER/wkhtmltox/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
+export WKHTMLTOPDF_CMD=$WKHTMLTOX_FOLDER/wkhtmltox/bin/wkhtmltopdf
 
