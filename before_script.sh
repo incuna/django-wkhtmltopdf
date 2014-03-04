ee307291f010936ced6bcd5c2eb999d7baf819b7
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-03-04 14:47:31 -0200

    Disable instaling openssl, build-ess...
    Disable instaling openssl build-essential xorg libssl-dev
    
    These libraries are really necessary?

--- before_script.sh
+++ before_script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 WKHTMLTOX_FOLDER="`pwd`/wkhtmltox_folder"
 
-echo '## Installing dependencies'
+#echo '## Installing dependencies'
 #sudo apt-get install -y openssl build-essential xorg libssl-dev
 echo '## Downloading wkhtmltox'
 wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.0/wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz
 mkdir -p $WKHTMLTOX_FOLDER
-echo '## Extracting wkhtmltox into $WKHTMLTOX_FOLDER'
+echo "## Extracting wkhtmltox into $WKHTMLTOX_FOLDER"
 tar xvJf wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz -C $WKHTMLTOX_FOLDER
 #sudo chown root:root wkhtmltox
 #sudo mv $WKHTMLTOX_FOLDER/wkhtmltox/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
