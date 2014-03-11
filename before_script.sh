4fb1f76d15ea609ec77ed8318ef0a6bf75880a9c
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-03-11 10:40:33 -0300

    update wkhtmltopdf binary to 0.12.0 version on before_script.sh

--- before_script.sh
+++ before_script.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+WKHTMLTOX_FOLDER="`pwd`/wkhtmltox_folder"
 
+echo '## Installing dependencies'
 sudo apt-get install -y openssl build-essential xorg libssl-dev
-wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2
-tar xvjf wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2
-sudo chown root:root wkhtmltopdf-amd64
-sudo mv wkhtmltopdf-amd64 /usr/bin/wkhtmltopdf
-export WKHTMLTOPDF_CMD=/usr/bin/wkhtmltopdf
+echo '## Downloading wkhtmltopdf 0.12.0'
+wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.0/wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz
+mkdir -p $WKHTMLTOX_FOLDER
+echo "## Extracting wkhtmltox into $WKHTMLTOX_FOLDER"
+tar xvJf wkhtmltox-linux-amd64_0.12.0-03c001d.tar.xz -C $WKHTMLTOX_FOLDER
+export WKHTMLTOPDF_CMD=$WKHTMLTOX_FOLDER/wkhtmltox/bin/wkhtmltopdf
+
