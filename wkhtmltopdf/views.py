95ac6164b4dd4add3268488d77cb82f9ddd50c88
David Sauve <david.sauve@bag-of-holding.com>
2014-03-19 12:39:03 -0700

    Ignore non-ascii characters in call to `make_absolute_paths`
    Ignore non-ascii characters in call to `make_absolute_paths`.  This is required to prevent `make_absolute_paths` from raising an exception when it encounters values it can't properly convert.

--- wkhtmltopdf/views.py
+++ wkhtmltopdf/views.py
@@ -75,7 +75,7 @@ class PDFTemplateResponse(TemplateResponse, PDFResponse):
         context = self.resolve_context(self.context_data)
 
         content = smart_str(template.render(context))
-        content = make_absolute_paths(content)
+        content = make_absolute_paths(unicode(content, 'ascii', 'ignore'))
 
         tempfile = NamedTemporaryFile(mode=mode, bufsize=bufsize,
                                       suffix=suffix, prefix=prefix,
