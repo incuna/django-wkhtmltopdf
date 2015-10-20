8224c46c84bc0d25c36d49d13610e8c4d02e2c5c
Tim Bell <tim@oodar.com.au>
2015-10-21 09:35:36 +1100

    Enable tests to run with Django >= 1.7 and silence warnings

--- wkhtmltopdf/tests/run.py
+++ wkhtmltopdf/tests/run.py
@@ -2,10 +2,12 @@
 import os
 import sys
 
+import django
 from django.conf import settings
 
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
+sys.path.insert(0, os.getcwd())
 
 settings.configure(
     DEBUG=True,
@@ -19,12 +21,22 @@ settings.configure(
         'wkhtmltopdf.tests',
         'wkhtmltopdf',
     ),
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+    ),
     MEDIA_ROOT=os.path.join(DIRNAME, 'media'),
     MEDIA_URL='/media/',
     STATIC_ROOT=os.path.join(DIRNAME, 'static'),
     STATIC_URL='/static/',
     WKHTMLTOPDF_DEBUG=True,
 )
+
+try:
+    django.setup()
+except AttributeError:
+    pass  # Django < 1.7; okay to ignore
+
 
 try:
     from django.test.runner import DiscoverRunner
