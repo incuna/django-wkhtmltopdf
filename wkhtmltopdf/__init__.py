8cc3363bf79fb5bf168457d4c936c01f6c58907f
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2016-05-01 19:17:39 -0300

    not expect django is previously installed
    https://github.com/incuna/django-wkhtmltopdf/issues/105

--- wkhtmltopdf/__init__.py
+++ wkhtmltopdf/__init__.py
@@ -1,6 +1,9 @@
 import os
-if 'DJANGO_SETTINGS_MODULE' in os.environ:
-    from .utils import *
+try:
+    if 'DJANGO_SETTINGS_MODULE' in os.environ:
+        from .utils import *
+except ImportError:
+    pass
 
 __author__ = 'Incuna Ltd'
 __version__ = '3.0.0'
